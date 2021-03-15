015d3df91e9758417c2a3e4315370ad6db69b7ad
logan moffett <8loganm@gmail.com>
2021-03-15 11:23:42 -0600

    zombie's init now works

--- game/create_zombie.py
+++ game/create_zombie.py
@@ -8,9 +8,9 @@ import math
 class Create_zombie(arcade.Sprite):
     """This class will allow us to create basic zombies with ease"""
 
-    #def __init__(self):
-        #super().__init__()
-        ##self.set_sprite(":resources:images/animated_characters/zombie/zombie_idle.png")
+    def __init__(self, sprite, scalling):
+        super().__init__(sprite, scalling)
+        #self.set_sprite(":resources:images/animated_characters/zombie/zombie_idle.png")
         #self.set_position(Point(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT))
     def update(self):
         """ Move the player """
