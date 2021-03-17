a4db081a61217f994612762618c1773fd36f06c8
logan moffett <8loganm@gmail.com>
2021-03-17 17:19:13 -0600

    Start screen

--- __main__.py
+++ __main__.py
@@ -1,4 +1,16 @@
 from game.director import Director
+from game.main_menu import main_menu
+from game import constants
+import arcade
+#window = Director()
+#window.execute()
 
-window = Director()
-window.execute()
+SCREEN_WIDTH = constants.SCREEN_WIDTH
+SCREEN_HEIGHT = constants.SCREEN_HEIGHT
+SCREEN_TITLE = constants.SCREEN_TITLE
+
+window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+start_view = main_menu()
+window.show_view(start_view)
+#start_view.setup()
+arcade.run()
